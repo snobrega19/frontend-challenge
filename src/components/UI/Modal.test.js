8d7eb80be422c8418c3d5cc7a174bcb54ad9762f
snobrega19 <s.nobrega@kigroup.de>
2022-07-07 12:19:38 +0100

    Modal.js test click on cancel button and on confirm button

--- src/components/UI/Modal.test.js
+++ src/components/UI/Modal.test.js
@@ -1,28 +1,24 @@
 import React from "react";
-import {
-  render,
-  screen,
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Modal from "./Modal";
+const message = "This is a test message";
 
 test("Shows a modal with a specific message", () => {
-  const message = "This is a test message";
   render(<Modal show={true} title="Test modal" message={message} />);
   expect(screen.getByText(/test modal/i)).toBeInTheDocument();
   expect(screen.getByTestId("modal-message")).toHaveTextContent(message);
 });
 
-// test("Click on 'no' button and close modal", async () => {
-//   let showModal = true;
-//   render(
-//     <Modal
-//       show={showModal}
-//       title="Test modal"
-//       onCancelClick={() => (showModal = false)}
-//     />
-//   );
-//   fireEvent.click(screen.getByRole("button", { name: /no/i }));
+test("Click on 'no' button", async () => {
+  const closeModal = jest.fn();
+  render(<Modal show={true} title="Test modal" onCancelClick={closeModal} />);
+  fireEvent.click(screen.getByRole("button", { name: /no/i }));
+  expect(closeModal).toHaveBeenCalled();
+});
 
-//   expect(screen.getByText(/test modal/i)).not.toBeInTheDocument();
-//   screen.debug();
-// });
+test("Click on 'yes' button", async () => {
+  const closeModal = jest.fn();
+  render(<Modal show={true} title="Test modal" onButtonClick={closeModal} />);
+  fireEvent.click(screen.getByRole("button", { name: /yes/i }));
+  expect(closeModal).toHaveBeenCalled();
+});
