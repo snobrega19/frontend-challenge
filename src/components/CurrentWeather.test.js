337d0524aa90f0366b16bd436cc27e57f971a79b
snobrega19 <s.nobrega@kigroup.de>
2022-07-07 14:27:11 +0100

    Add test to StatusBar.js to show a status bar with danger variant.

---
+++ src/components/CurrentWeather.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {
+  render,
+  screen,
+} from "@testing-library/react";
+import Modal from "./Modal";
+
+test("Shows a modal with a specific message", () => {
+  
+});
+
+
--- src/components/UI/StatusBar.test.js
+++ src/components/UI/StatusBar.test.js
@@ -1,8 +1,5 @@
 import React from "react";
-import {
-  render,
-  screen,
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import StatusBar from "./StatusBar";
 
 test("Shows a bar with a success message", () => {
@@ -11,4 +8,12 @@ test("Shows a bar with a success message", () => {
   const statusBar = screen.getByTestId("status-bar");
   expect(statusBar).toHaveTextContent(message);
   expect(statusBar).toHaveClass("alert-success");
+});
+
+test("Shows a bar with a message with danger variant", () => {
+  const message = "This is a error message";
+  render(<StatusBar variant="danger" message={message} />);
+  const statusBar = screen.getByTestId("status-bar");
+  expect(statusBar).toHaveTextContent(message);
+  expect(statusBar).toHaveClass("alert-danger");
 });
